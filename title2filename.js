b9cda1846d5e18d6529a2c382660999393c5bfa7
Lewis Bolaton <lewisbolaton@gmail.com>
2024-04-02 11:12:44 +0800

    Removed redundant files with js extension.

--- title2filename.js
+++
@@ -1,9 +0,0 @@
-function title2filename(separator, ...fields) {
-    for (let i = 0; i < fields.length; i += 1) {
-        if (typeof fields[i] === 'string') {
-            console.log('Pass main arg ONLY');
-        } else {
-            console.log('Pass main arg and optional');
-        }
-    }
-}
