f1043394a68f36e3ca538a223d38f3b4d30a878b
Lewis Bolaton <lewisbolaton@gmail.com>
2024-04-02 19:40:37 +0800

    Changed title2filename definition

--- main.mjs
+++ main.mjs
@@ -1,5 +1,5 @@
 import title2filename from './title2filename.mjs';
 import convertChar from './convertChar.mjs';
 
-title2filename('_', 'Title: Piece\'s Subtitle', ['Hello! MAR-2023', '-']);
+console.log(title2filename('_', 'Juan\'s Document', ['2023-10-21', '-'], '"Hello!" in Multiple Languages'));
 [['3', 'C'], [' ', 'n'], ['\'', ' '], ['_', 't']].forEach(el => console.log(convertChar(...el)));
--- title2filename.mjs
+++ title2filename.mjs
@@ -1,14 +1,18 @@
 import Field from './Field.mjs'
 
 export default function title2filename(separator, ...fields) {
+  var fieldStrings = [];
+
   for (let i = 0; i < fields.length; i += 1) {
 	  var f;
-      if (typeof fields[i] === 'string') {
-      	f = new Field(fields[i]);
-      	console.log(f.convert());
-      } else {
-      	f = new Field(fields[i][0]);
-      	console.log(f.convert(fields[i][1]));
-      }
+    if (typeof fields[i] === 'string') {
+    	f = new Field(fields[i]);
+    	fieldStrings.push(f.convert());
+    } else {
+    	f = new Field(fields[i][0]);
+    	fieldStrings.push(f.convert(fields[i][1]));
+    }
   }
+  
+  return fieldStrings.join(separator);
 }
