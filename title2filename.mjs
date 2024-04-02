78672ccad470672d1e9a0dd468255d9e803432c7
Lewis Bolaton <lewisbolaton@gmail.com>
2024-04-02 18:48:21 +0800

    Fixed inconsistent indentation spacing.

--- title2filename.mjs
+++ title2filename.mjs
@@ -1,14 +1,14 @@
 import Field from './Field.mjs'
 
 export default function title2filename(separator, ...fields) {
-    for (let i = 0; i < fields.length; i += 1) {
-    	var f;
-        if (typeof fields[i] === 'string') {
-        	f = new Field(fields[i]);
-        	console.log(f.convert());
-        } else {
-        	f = new Field(fields[i][0]);
-        	console.log(f.convert(fields[i][1]));
-        }
-    }
+  for (let i = 0; i < fields.length; i += 1) {
+	  var f;
+      if (typeof fields[i] === 'string') {
+      	f = new Field(fields[i]);
+      	console.log(f.convert());
+      } else {
+      	f = new Field(fields[i][0]);
+      	console.log(f.convert(fields[i][1]));
+      }
+  }
 }
