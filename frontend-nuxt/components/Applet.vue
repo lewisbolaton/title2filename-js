0b8c41dcdd5a44796d8a35581fcc84c215c04a27
Lewis Bolaton <lewisbolaton@gmail.com>
2024-04-05 14:25:26 +0800

    Initialized Applet component

---
+++ frontend-nuxt/components/Applet.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="applet">
+    <div class="entries">
+      <span>Entry</span>
+      <span>Entry</span>
+      <span>Entry</span>
+      <button>add_field</button>
+    </div>
+
+    <button>convert</button>
+
+    <div class="results" v-if="isConverted">
+      <div class="clipboard">
+        <input readonly type="text">
+        <button>copy</button>
+      </div>
+      <button>convert_new</button>
+    </div>
+  </div>
+</template>
+
+<script lang="js">
+  export default {
+    data () {
+      return { isConverted: false }
+    }
+  }
+</script>
+
+<style>
+.applet {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  
+  gap: 10px;
+}
+
+.entries {
+  background-color: green;
+  
+  display: flex;
+  flex-direction: column;
+  align-items: center;  
+  gap: 10px;
+  
+  padding: 10px;
+}
+</style>
